53fc349985e7ff9eef37549a3dba6daf364be7cb
Leo Ris <ristanovski@hotmail.com>
2022-01-12 22:51:02 +0100

    Adding a MessageBox in case LastName doesn't exist in Db.

--- DapperDataAccessDemo/DataAccess.cs
+++ DapperDataAccessDemo/DataAccess.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Dapper;
 using System.Data;
+using System.Windows.Forms;
 
 namespace DapperDataAccessDemo
 {
@@ -15,7 +16,13 @@ namespace DapperDataAccessDemo
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.ConnectionValue("Sample")))
             {
                 //return connection.Query<Person>($"SELECT * FROM People WHERE LastName = '{ lastName }'").ToList();
-                return connection.Query<Person>("dbo.People_GetByLastName @LastName", new { LastName = lastName }).ToList();
+                var output = connection.Query<Person>("dbo.People_GetByLastName @LastName", new { LastName = lastName }).ToList();
+
+                if(output.Count == 0)
+                {
+                    MessageBox.Show($"Last name: '{lastName}' was not found.");
+                }
+                return output;
             }
         }
     }
